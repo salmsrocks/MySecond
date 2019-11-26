70ecca5306992e74869eca268d379bba29565319
salmsrocks <58145915+salmsrocks@users.noreply.github.com>
2019-11-26 16:10:55 +0530

    Update film-ratings-app-service.tf

--- film-ratings-app-service.tf
+++ film-ratings-app-service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "film_ratings_app_service" {
   iam_role        = "${aws_iam_role.ecs-service-role.name}"
   cluster         = "${aws_ecs_cluster.film_ratings_ecs_cluster.id}"
   task_definition = "${aws_ecs_task_definition.film_ratings_app.family}:${max("${aws_ecs_task_definition.film_ratings_app.revision}", "${data.aws_ecs_task_definition.film_ratings_app.revision}")}"
-  depends_on      = [ "aws_ecs_service.film_ratings_db_service"]
+  #depends_on      = [ "aws_ecs_service.film_ratings_db_service"]
   desired_count   = "${var.desired_capacity}"
   deployment_minimum_healthy_percent = "50"
   deployment_maximum_percent = "100"
