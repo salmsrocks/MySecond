57032c74ce3fe6651b77949d36c260c5e565c249
salmsrocks <58145915+salmsrocks@users.noreply.github.com>
2020-02-25 19:25:31 +0530

    Update ecs-instance-role.tf

--- ecs-instance-role.tf
+++ ecs-instance-role.tf
@@ -21,6 +21,13 @@ resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
 }
 
 
-
+resource "aws_iam_instance_profile" "ecs-instance-profile" {
+  name = "ecs-instance-profile"
+  path = "/"
+  role = "${aws_iam_role.ecs-instance-role.id}"
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+}
 
 
